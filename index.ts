a564c31caf65ff5182f5cecb2d5119e3b1d9ae6d
unknown <a00516283@gmail.com>
2019-04-23 02:14:31 -0500

    Hola

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import empresaRoutes from "./routes/empresa";
 import userRoutes from "./routes/usuario";
 import espacioRoutes from "./routes/espacio";
+import {Request, Response} from "express";
 
 const server  = Server.instance;
 //mecatronica20
@@ -27,7 +28,9 @@ server.app.use(cors({
     origin: true,
     credentials: true
 }));
-
+server.app.get("/", (req: Request, res: Response, next: Function)=>{
+    res.send("Hola amigos");
+});
 server.app.use("/empresas", empresaRoutes);
 server.app.use("/usuarios", userRoutes);
 server.app.use("/espacios", espacioRoutes);
